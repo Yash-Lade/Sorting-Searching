3a31fc43dcf18b31f7becfe1507917ee31e007bd
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-12-04 22:14:02 +0530

    Add files via upload

---
+++ bubbleash.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a[10],b,i,j,n;
+	
+	cout<<"Enter the no. of elements :";
+	cin>>n;
+	
+	cout<<"Enter values in array :";
+	for(i=1;i<=n;i++)
+	{
+		cin>>a[i];
+	}
+	
+	cout<<"The elements before sorting are :";
+	for(i=1;i<=n;i++)
+	{
+		cout<<a[i];
+	}
+	
+	cout<<"\nArray after sorting :";
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-1;j++)
+		{
+			if(a[j]>a[j+1])
+			swap(a[j],a[j+1]);
+		}
+	}
+    for(i=1;i<=n;i++)
+    cout<<a[i];
+}
